d32c3f7d2cdd27463e6ba0600768f9736906a7d3
Sabina Malajter <malajterovasabina@gmail.com>
2024-11-14 11:49:05 +0100

    "Create Hero with name and start the game"

--- src/main/java/sk/malajter/Main.java
+++ src/main/java/sk/malajter/Main.java
@@ -1,8 +1,21 @@
 package sk.malajter;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Gladiatus game!");
+        System.out.println("Enter your name: ");
+
+        final String name = scanner.nextLine();
+        final Hero hero = new Hero(name);
+        System.out.println("Hello " + hero.getName() + " . Let's start the game!");
+
+
+
+
     }
 }
